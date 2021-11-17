ba9ee2c82135227b3c0e58b33c58c45889190ad6
Tommy Tran <tranman100598@gmail.com>
2021-11-17 15:51:55 -0700

    Revert back before bug

--- src/main/java/com/example/hospitalapplication/Connect.java
+++ src/main/java/com/example/hospitalapplication/Connect.java
@@ -7,8 +7,8 @@ public class Connect {
     // SQLite
     public static Connection connect() {
         // replace "url" variable with your own database path
-        //String url = "jdbc:sqlite:/Users/Tommy/Documents/asu/CSE360/database/hospital.db";
-        String url = "jdbc:sqlite:/Users/samqu/Documents/Databases/hospital project/hospital.db";
+        String url = "jdbc:sqlite:/Users/Tommy/Documents/asu/CSE360/database/hospital.db";
+        //String url = "jdbc:sqlite:/Users/samqu/Documents/Databases/hospital project/hospital.db";
         Connection conn = null;
         try {
             // create a connection to the database
